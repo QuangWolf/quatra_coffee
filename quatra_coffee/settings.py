da6515b8b9d3d7b3f0ec1582763b0f321b418a15
QuangWolf <congquang18082004@gmail.com>
2025-07-27 03:57:43 +0700

    Add render config

--- quatra_coffee/settings.py
+++ quatra_coffee/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a78l(ay4wbpmflrh)lr1!wy4!i3q0gni@g#3tqi6q5)@*y_i0e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 CSRF_TRUSTED_ORIGINS = [
     'http://192.168.52.20',
 ]
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
